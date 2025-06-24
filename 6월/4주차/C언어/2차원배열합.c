2231f5385862132641b1d8a01667d81944c7a57b
김은호 <kehlove09@gmail.com>
2025-06-25 00:13:35 +0900

    2차원배열 대각선 합

--- 6월/4주차/C언어/2차원배열합.c
+++ 6월/4주차/C언어/2차원배열합.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+
 int main(void)
 {
-    int a[2][2];
+    int n;
+    printf("배열 크기를 입력하세요 (최대 10): ");
+    scanf("%d", &n);
+
+    int a[10][10];
+
+    printf("%d x %d 배열의 값을 입력하세요:\n", n, n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i][i];
+    }
+
+    printf("대각선 합: %d\n", sum);
+    return 0;
 }
