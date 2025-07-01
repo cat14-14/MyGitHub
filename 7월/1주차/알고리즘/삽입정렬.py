98b2e40902c08a8cc72f621a5a3e5bf031c0acb4
김은호 <kehlove09@gmail.com>
2025-07-01 13:39:35 +0900

    삽입정렬

--- 7월/1주차/알고리즘/삽입정렬.py
+++ 7월/1주차/알고리즘/삽입정렬.py
@@ -1,1 +1,13 @@
 l = input().split()
+result = []
+
+while l:
+    v = l.pop(0)
+    id = len(result)
+    for i in range(len(result)):
+        if v < result[i]:
+            id = i
+            break
+    result.insert(id, v)
+
+print(result)
