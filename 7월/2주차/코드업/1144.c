ab7133b8f53aa1ed783d0bf92cbfce027b2de20f
김은호 <kehlove09@gmail.com>
2025-07-10 11:29:10 +0900

    111111

---
+++ 7월/2주차/코드업/1144.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d",&a,&b);
+    printf("%d",a|b);
+}
---
+++ 7월/2주차/코드업/1149.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d",&a,&b);
+    if(a>b)printf("%d",a);
+    else printf("%d",b);
+}
