d10026bb3a879927ff543a1cff77e3bae0fe9d64
cat14-14 <kehlove09@gmail.com>
2025-09-11 07:29:26 +0900

    3132132

--- 8월/자료구조/1번문제.c
+++ 8월/자료구조/1번문제.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void) {
     int n;
     scanf("%d", &n);
-
     int *a;
     a = (int*)malloc(sizeof(int) * n);
-
     for (int i = 0; i < n; ++i) {
         scanf("%d", &a[i]);
     }
-
     long long sum = 0;
     for (int i = 0; i < n; i++) sum += a[i];
-
     printf("%lld\n", sum);
-
     free(a);
     return 0;
 }
